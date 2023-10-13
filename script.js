e79531d902181db9862921b3fc4bed9b1ae41a37
vanessa moultrie <imvanessa16@gmail.com>
2023-10-12 22:37:21 -0400

    9 am restores properly from local storage

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  let nineAmAppointment = localStorage.getItem("hour-9");
+  $("#hour-9 .description").val(nineAmAppointment);
   //
   // TODO: Add code to display the current date in the header of the page.
   let today=dayjs().format("dddd, MMMM D, YYYY");
