b40c11d9c3d07ebbdb760c6158bdc5f84bc2e250
vanessa moultrie <imvanessa16@gmail.com>
2023-10-12 22:32:03 -0400

    added changes to script.js to save to local storage

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $(".saveBtn").on("click",saveAppointment);
+
+  function saveAppointment(){
+    let appointment = $(this).siblings('.description').val();
+    console.log("appointment= ", appointment);
+    let appointmentHour = $(this).parent().attr("id");
+    console.log("appointmentHour= ", appointmentHour);
+
+    localStorage.setItem(appointmentHour, appointment);
+  }
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
